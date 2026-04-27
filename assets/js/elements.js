7eadebabfe82787f1adaff73a4719106d276e2e0
David Berkheimer <berkheimer112@atlanticbb.net>
2026-04-27 18:05:28 -0400

    needs style

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -1,6 +1,5 @@
 class Elements {
   constructor() {
-    // select the lists
     this.showTimeHour = document.querySelector("#showTimeHour");
     this.showTimeMinute = document.querySelector("#showTimeMinute");
     this.movieLengthHour = document.querySelector("#movieLengthHour");
@@ -8,5 +7,15 @@ class Elements {
     this.previewLengthMinute = document.querySelector("#previewLengthMinute");
     this.calculateButton = document.querySelector("#calculateButton");
     this.endTimeOutput = document.querySelector("#endTimeOutput");
+    this.movieTitle = document.querySelector("#movieTitle");
+
+    this.showTimeHour2 = document.querySelector("#showTimeHour2");
+    this.showTimeMinute2 = document.querySelector("#showTimeMinute2");
+    this.movieLengthHour2 = document.querySelector("#movieLengthHour2");
+    this.movieLengthMinute2 = document.querySelector("#movieLengthMinute2");
+    this.previewLengthMinute2 = document.querySelector("#previewLengthMinute2");
+    this.calculateButton2 = document.querySelector("#calculateButton2");
+    this.endTimeOutput2 = document.querySelector("#endTimeOutput2");
+    this.movieTitle2 = document.querySelector("#movieTitle2");
   }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -44,5 +44,52 @@ el.calculateButton.addEventListener("click", (e) => {
     totalMinutes = "0" + totalMinutes;
   }
 
-  el.endTimeOutput.innerHTML = `<p> Movie ends at ${totalHours}:${totalMinutes}</p>`;
+  if (el.movieTitle.value === "") {
+    el.endTimeOutput.innerHTML = `<p> Movie ends at ${totalHours}:${totalMinutes}</p>`;
+  } else {
+    el.endTimeOutput.innerHTML = `<p> ${el.movieTitle.value} ends at ${totalHours}:${totalMinutes}</p>`;
+  }
+});
+
+el.calculateButton2.addEventListener("click", (e) => {
+  let totalHours = 0;
+  let totalMinutes = 0;
+
+  // and show time and show length
+  totalHours =
+    Number(el.showTimeHour2.value) + Number(el.movieLengthHour2.value);
+  totalMinutes =
+    Number(el.showTimeMinute2.value) + Number(el.movieLengthMinute2.value);
+
+  // Check for minutes over or equal to 60
+  if (totalMinutes >= 60) {
+    totalHours = totalHours + 1;
+    totalMinutes = totalMinutes - 60;
+  }
+
+  // Add preview length to totals
+  totalMinutes = totalMinutes + Number(el.previewLengthMinute2.value);
+
+  // Second check for minutes over or equal to 60
+  if (totalMinutes >= 60) {
+    totalHours = totalHours + 1;
+    totalMinutes = totalMinutes - 60;
+  }
+
+  // Check if Hours > 12 { timeHours -12}
+  if (totalHours > 12) {
+    totalHours = totalHours - 12;
+  }
+
+  // output
+
+  if (totalMinutes < 10) {
+    totalMinutes = "0" + totalMinutes;
+  }
+
+  if (el.movieTitle2.value === "") {
+    el.endTimeOutput2.innerHTML = `<p> Movie ends at ${totalHours}:${totalMinutes}</p>`;
+  } else {
+    el.endTimeOutput2.innerHTML = `<p> ${el.movieTitle2.value} ends at ${totalHours}:${totalMinutes}</p>`;
+  }
 });
