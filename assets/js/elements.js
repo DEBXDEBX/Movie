d498018cdb0275c8651eed72e1f3fe08051fc047
David Berkheimer <berkheimer112@atlanticbb.net>
2026-05-02 12:08:14 -0400

    added more movies

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -17,5 +17,32 @@ class Elements {
     this.calculateButton2 = document.querySelector("#calculateButton2");
     this.endTimeOutput2 = document.querySelector("#endTimeOutput2");
     this.movieTitle2 = document.querySelector("#movieTitle2");
+
+    this.showTimeHour3 = document.querySelector("#showTimeHour3");
+    this.showTimeMinute3 = document.querySelector("#showTimeMinute3");
+    this.movieLengthHour3 = document.querySelector("#movieLengthHour3");
+    this.movieLengthMinute3 = document.querySelector("#movieLengthMinute3");
+    this.previewLengthMinute3 = document.querySelector("#previewLengthMinute3");
+    this.calculateButton3 = document.querySelector("#calculateButton3");
+    this.endTimeOutput3 = document.querySelector("#endTimeOutput3");
+    this.movieTitle3 = document.querySelector("#movieTitle3");
+
+    this.showTimeHour4 = document.querySelector("#showTimeHour4");
+    this.showTimeMinute4 = document.querySelector("#showTimeMinute4");
+    this.movieLengthHour4 = document.querySelector("#movieLengthHour4");
+    this.movieLengthMinute4 = document.querySelector("#movieLengthMinute4");
+    this.previewLengthMinute4 = document.querySelector("#previewLengthMinute4");
+    this.calculateButton4 = document.querySelector("#calculateButton4");
+    this.endTimeOutput4 = document.querySelector("#endTimeOutput4");
+    this.movieTitle4 = document.querySelector("#movieTitle4");
+
+    this.showTimeHour5 = document.querySelector("#showTimeHour5");
+    this.showTimeMinute5 = document.querySelector("#showTimeMinute5");
+    this.movieLengthHour5 = document.querySelector("#movieLengthHour5");
+    this.movieLengthMinute5 = document.querySelector("#movieLengthMinute5");
+    this.previewLengthMinute5 = document.querySelector("#previewLengthMinute5");
+    this.calculateButton5 = document.querySelector("#calculateButton5");
+    this.endTimeOutput5 = document.querySelector("#endTimeOutput5");
+    this.movieTitle5 = document.querySelector("#movieTitle5");
   }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,10 @@ function startUp() {}
 
 // create elements object
 const el = new Elements();
+let btnAudio = document.querySelector("#btnAudio");
 
 el.calculateButton.addEventListener("click", (e) => {
+  btnAudio.play();
   let totalHours = 0;
   let totalMinutes = 0;
 
@@ -54,6 +56,7 @@ el.calculateButton.addEventListener("click", (e) => {
 });
 
 el.calculateButton2.addEventListener("click", (e) => {
+  btnAudio.play();
   let totalHours = 0;
   let totalMinutes = 0;
 
@@ -93,8 +96,148 @@ el.calculateButton2.addEventListener("click", (e) => {
     el.endTimeOutput2.innerHTML = `<p> Movie ends at ${totalHours}:${totalMinutes}</p>`;
   } else {
     let str = el.movieTitle2.value;
-    console.log(str);
+
     str = str.replace(/\b\w/g, (match) => match.toUpperCase());
     el.endTimeOutput2.innerHTML = `<p> ${str} ends at ${totalHours}:${totalMinutes}</p>`;
+  }
+});
+
+el.calculateButton3.addEventListener("click", (e) => {
+  btnAudio.play();
+  let totalHours = 0;
+  let totalMinutes = 0;
+
+  // and show time and show length
+  totalHours =
+    Number(el.showTimeHour3.value) + Number(el.movieLengthHour3.value);
+  totalMinutes =
+    Number(el.showTimeMinute3.value) + Number(el.movieLengthMinute3.value);
+
+  // Check for minutes over or equal to 60
+  if (totalMinutes >= 60) {
+    totalHours = totalHours + 1;
+    totalMinutes = totalMinutes - 60;
+  }
+
+  // Add preview length to totals
+  totalMinutes = totalMinutes + Number(el.previewLengthMinute3.value);
+
+  // Second check for minutes over or equal to 60
+  if (totalMinutes >= 60) {
+    totalHours = totalHours + 1;
+    totalMinutes = totalMinutes - 60;
+  }
+
+  // Check if Hours > 12 { timeHours -12}
+  if (totalHours > 12) {
+    totalHours = totalHours - 12;
+  }
+
+  // output
+
+  if (totalMinutes < 10) {
+    totalMinutes = "0" + totalMinutes;
+  }
+
+  if (el.movieTitle3.value === "") {
+    el.endTimeOutput3.innerHTML = `<p> Movie ends at ${totalHours}:${totalMinutes}</p>`;
+  } else {
+    let str = el.movieTitle3.value;
+
+    str = str.replace(/\b\w/g, (match) => match.toUpperCase());
+    el.endTimeOutput3.innerHTML = `<p> ${str} ends at ${totalHours}:${totalMinutes}</p>`;
+  }
+});
+
+el.calculateButton4.addEventListener("click", (e) => {
+  btnAudio.play();
+  let totalHours = 0;
+  let totalMinutes = 0;
+
+  // and show time and show length
+  totalHours =
+    Number(el.showTimeHour4.value) + Number(el.movieLengthHour4.value);
+  totalMinutes =
+    Number(el.showTimeMinute4.value) + Number(el.movieLengthMinute4.value);
+
+  // Check for minutes over or equal to 60
+  if (totalMinutes >= 60) {
+    totalHours = totalHours + 1;
+    totalMinutes = totalMinutes - 60;
+  }
+
+  // Add preview length to totals
+  totalMinutes = totalMinutes + Number(el.previewLengthMinute4.value);
+
+  // Second check for minutes over or equal to 60
+  if (totalMinutes >= 60) {
+    totalHours = totalHours + 1;
+    totalMinutes = totalMinutes - 60;
+  }
+
+  // Check if Hours > 12 { timeHours -12}
+  if (totalHours > 12) {
+    totalHours = totalHours - 12;
+  }
+
+  // output
+
+  if (totalMinutes < 10) {
+    totalMinutes = "0" + totalMinutes;
+  }
+
+  if (el.movieTitle4.value === "") {
+    el.endTimeOutput4.innerHTML = `<p> Movie ends at ${totalHours}:${totalMinutes}</p>`;
+  } else {
+    let str = el.movieTitle4.value;
+
+    str = str.replace(/\b\w/g, (match) => match.toUpperCase());
+    el.endTimeOutput4.innerHTML = `<p> ${str} ends at ${totalHours}:${totalMinutes}</p>`;
+  }
+});
+el.calculateButton5.addEventListener("click", (e) => {
+  btnAudio.play();
+  let totalHours = 0;
+  let totalMinutes = 0;
+
+  // and show time and show length
+  totalHours =
+    Number(el.showTimeHour5.value) + Number(el.movieLengthHour5.value);
+  totalMinutes =
+    Number(el.showTimeMinute5.value) + Number(el.movieLengthMinute5.value);
+
+  // Check for minutes over or equal to 60
+  if (totalMinutes >= 60) {
+    totalHours = totalHours + 1;
+    totalMinutes = totalMinutes - 60;
+  }
+
+  // Add preview length to totals
+  totalMinutes = totalMinutes + Number(el.previewLengthMinute5.value);
+
+  // Second check for minutes over or equal to 60
+  if (totalMinutes >= 60) {
+    totalHours = totalHours + 1;
+    totalMinutes = totalMinutes - 60;
+  }
+
+  // Check if Hours > 12 { timeHours -12}
+  if (totalHours > 12) {
+    totalHours = totalHours - 12;
+  }
+
+  // output
+
+  if (totalMinutes < 10) {
+    totalMinutes = "0" + totalMinutes;
+  }
+
+  if (el.movieTitle5.value === "") {
+    el.endTimeOutput5.innerHTML = `<p> Movie ends at ${totalHours}:${totalMinutes}</p>`;
+  } else {
+    let str = el.movieTitle5.value;
+
+    str = str.replace(/\b\w/g, (match) => match.toUpperCase());
+    el.endTimeOutput5.innerHTML = `<p> ${str} ends at ${totalHours}:${totalMinutes}</p>`;
   }
 });
